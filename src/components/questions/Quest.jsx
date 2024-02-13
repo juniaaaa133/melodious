701dba9a7a845a6b97db387d944b1737f8accfe4
Reinnn <xopeofhopeness2004@gmail.com>
2024-02-13 12:53:42 +0630

    Localstorage issue fix

--- src/components/questions/Quest.jsx
+++ src/components/questions/Quest.jsx
@@ -2,40 +2,42 @@
 
 import { forms } from '@/data'
 import ButtonX from '@/elements/button/ButtonX'
-import ButtonY from '@/elements/button/ButtonY'
-import { redirect } from 'next/navigation'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 const Quest = () => {
-let quests_from_storage = forms[parseInt(localStorage.getItem('quest_to_show'))] //Getting data from api by using localstorage conditional
 let initial_data = forms[0]
 
     let [quests,setQuests] = useState(null); //Push LS data to usestate . INITIALLY EMPTY ARRAY ;QUEST MUST NOT BE UNDEFINED!
     let router = useRouter();
 
     useEffect(()=>{
+  if(localStorage !== undefined){
     if(localStorage.getItem('quest_to_show') == null || parseInt(localStorage.getItem('quest_to_show') == NaN)){
         localStorage.setItem('quest_to_show',0); //Initial state or the time when user finish and restart quests.
         console.log('is null')
 
         setQuests(initial_data) //Everytime window refresh , data from LS will be stored in usestate;
     }else {
-       setQuests(quests_from_storage) //Everytime window refresh , data from LS will be stored in usestate;
+       setQuests(forms[parseInt(localStorage.getItem('quest_to_show'))]) //Everytime window refresh , data from LS will be stored in usestate;
        console.log('is not null') //Continuous state, remembering the quests in LS.
     }
-},[quests_from_storage]) //Everytime the client loads , window refresh , it works;
+  }
+},[forms[parseInt(localStorage.getItem('quest_to_show'))]]) //Everytime the client loads , window refresh , it works;
 
 useEffect(()=>{
+if(localStorage !== undefined){
     if(parseInt(localStorage.getItem('quest_to_show')) +1  == forms.length){ //Whenever data in usestate change, this will activate .
-    router.push('/playlist')  
-    }else{
-        return;
-  }
+        router.push('/playlist')  
+        }else{
+            return;
+      }
+}
 },[])
 
 
 let ChoiceFunction = (chosen_data) => { //When a user click one answer;
+  if(localStorage !== undefined){
     localStorage.setItem(`${parseInt(localStorage.getItem('quest_to_show') ) + 1 }`,chosen_data) //Storing chosen data in LS to filter the reslut
     if(parseInt(localStorage.getItem('quest_to_show'))+1 == forms.length){
     localStorage.setItem(`${parseInt(localStorage.getItem('quest_to_show') ) + 1 }`,chosen_data) //Storing chosen data in LS to filter the reslut
@@ -46,9 +48,11 @@ let ChoiceFunction = (chosen_data) => { //When a user click one answer;
     let next_quest_from_storage = forms[parseInt(localStorage.getItem('quest_to_show'))]; //Update LS length as ready made array for next question to push to usestate.
     setQuests(next_quest_from_storage); //Set updated data to usestate;
 }
+  }
     }
 
 let PreviousFunction = () => {
+ if(localStorage !== undefined){
     if(localStorage.getItem('quest_to_show') !== '0'){
         localStorage.setItem('quest_to_show',parseInt(localStorage.getItem('quest_to_show') -1));
         let next_quest_from_storage = forms[parseInt(localStorage.getItem('quest_to_show'))]; //Update LS length as ready made array for next question to push to usestate.
@@ -57,6 +61,7 @@ let PreviousFunction = () => {
         return;
         
     }
+ }
 }
 
          return (
